5bc8c360b807b5be249754da13f164504238ce18
Ali-Elouasbi <ali.elouasbi@mths.ca>
2024-10-09 11:16:47 -0400

    2-09

--- main.ts
+++ main.ts
@@ -5,4 +5,36 @@
  * This program ...
 */
 
-basic.showString('Hello, World!')
+// variables
+let randomNumber: number = 0
+
+randomNumber = -1
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+input.onGesture(Gesture.Shake, function () {
+    randomNumber = randint(0, 1)
+    basic.clearScreen()
+
+    // if randomNumber was 0
+    if (randomNumber == 0) {
+        basic.showIcon(IconNames.Square)
+    }
+
+    // if randomNumber was 1
+    if (randomNumber == 1) {
+        basic.showIcon(IconNames.SmallSquare)
+    }
+
+    // if randomNumber was 2
+    if (randomNumber == 2) {
+        basic.showIcon(IconNames.Scissors)
+    }
+    // pasue and show you are ready again
+    basic.pause(1000)
+    basic.showIcon(IconNames.Happy)
+
+    input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Yes)
+    
+})
